1f179e36798b0f18881f2442eb8949c5bbbd6b73
yuanshuo_wu <yuanshuowu200502@gmail.com>
2023-02-15 15:27:26 -0800

    Add an API router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api');
+
 
 // router.get('/hello/world', function(req, res) {
 //   res.cookie('XSRF-TOKEN', req.csrfToken());
@@ -13,5 +15,7 @@ router.get("/api/csrf/restore", (req, res) => {
       'XSRF-Token': csrfToken
     });
   });
+
+router.use('/api', apiRouter);
 
 module.exports = router;
